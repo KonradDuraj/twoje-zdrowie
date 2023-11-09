670e82967bc8c224649bce27727aa3a06564c2a8
KonradDuraj <konrad.m.duraj@gmail.com>
2023-11-09 18:30:34 +0100

    Added about template and view

--- booking/website/urls.py
+++ booking/website/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home.html', views.home, name='home'),
+    path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact')
 ]
